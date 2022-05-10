2474f5e8c2c9bbadf785b3b2971dd829f0ecfa09
Henrique Dos Santos Rodriguez <henrique_rz@hotmail.com>
2022-05-10 12:45:36 -0300

    teste

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,62 +1,20 @@
-/* let a = 10
-let b = 10
+// // console.log('Olá turma')
 
-console.log(b)
+// // const primeiroValor = 10
+// // const segundoValor = 20
 
-b = 5
-console.log(a, b)
+// // const resultado = primeiroValor + segundoValor + 10
+// // console.log (resultado)
 
-// Resultado da primeira é 10, da segunda é 10
+// // const resultadoo = 15 - 2 + 10
+// // console.log (resultadoo)
 
-let a = 10
-let b = 20
-c = a
-b = c
-a = b
+// const restoDaDivisao = 6 % 4
+// console.log(restoDaDivisao)
 
-console.log(a, b, c)
+const tetaUm = 3
+const tetaDois = 5
 
-// Todos são 10
+const resultado = (tetaUm * tetaDois) /2
+console.log(resultado)
 
-let p = prompt("Quantas horas você trabalha por dia?")
-let t = prompt("Quanto você recebe por dia?")
-alert(`Voce recebe ${t/p} por hora`)
-
-// Primeiramente o prompt = Abre uma caixa de diálogo no site
-
-const nome = null
-const idade = idade
-
-console.log(typeof nome, typeof idade)
-
-// Foi definido como objeto, por ainda não dei um valor a essa variável
-*/
-nome = prompt("Seu nome")
-idade = prompt("Sua idade")
-
-console.log(nome, idade)
-console.log(typeof nome, typeof idade)
-
-//As duas variaveis se tornaram String, pois foi digitado em uma caixa de dialogo
-
-alert(`Olá - ${nome} voce tem - ${idade} anos`)
-
-let perg1 = prompt("Voce esta bem?")
-let perg2 = prompt("Voce se alimentou hoje?")
-let perg3 = prompt("Voce tem grana?")
-
-alert(`Voce esta bem? - ${perg1}`)
-alert(`Voce se alimentou hoje? - ${perg2}`)
-alert(`Voce tem grana? - ${perg3}`)
-
-let a = 10
-let b = 25
-console.log("Valor inicial a", a)
-console.log("Valor inicial b", b)
-let c = a
-c = a
-b = c
-a = b
-
-console.log("O novo valor de a é", a)
-console.log("O novo valor de b é", b)
