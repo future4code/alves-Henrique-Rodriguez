dfdf44eaac2f2f5a86897a94112745a21f892b93
Henrique Dos Santos Rodriguez <henrique_rz@hotmail.com>
2022-05-17 14:16:12 -0300

    Condicionais maneiras arrumado

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -1,69 +1,69 @@
-// // Parte 1
-// // Exercício 1
-// const respostaDoUsuario = prompt("Digite o número que você quer testar")
-// const numero = Number(respostaDoUsuario)
+// Parte 1
+// Exercício 1
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
 
-// if (numero % 2 === 0) {
-//   console.log("Passou no teste.")
-// } else {
-//   console.log("Não passou no teste.")
-// }
-// // a) Ele compara e divide os números
-// // b) Para números pares
-// // c) Para números impares
+if (numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+// a) Ele compara e divide os números
+// b) Para números pares
+// c) Para números impares
 
-// // Exercício 2
-// let fruta = prompt("Escolha uma fruta")
-// let preco
-// switch (fruta) {
-//   case "Laranja":
-//     preco = 3.5
-//     break;
-//   case "Maçã":
-//     preco = 2.25
-//     break;
-//   case "Uva":
-//     preco = 0.30
-//     break;
-//   case "Pêra":
-//     preco = 5.5
-//     break; // BREAK PARA O ITEM c.
-//   default:
-//     preco = 5
-//     break;
-// }
-// console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
-// // a) Para mostrar qual a fruta tem na lista o valor
-// // b) "O preço da fruta  Maçã  é  R$  2.25"
-// // c) A mensagem seria "O preço da fruta  Pêra  é  R$  5"
+// Exercício 2
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+// a) Para mostrar qual a fruta tem na lista o valor
+// b) "O preço da fruta  Maçã  é  R$  2.25"
+// c) A mensagem seria "O preço da fruta  Pêra  é  R$  5"
 
-// // Exercício 3
-// const numero = Number(prompt("Digite o primeiro número."))
+// Exercício 3
+const numero = Number(prompt("Digite o primeiro número."))
 
-// if(numero > 0) {
-//   console.log("Esse número passou no teste")
-// 	let mensagem = "Essa mensagem é secreta!!!"
-// }
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
 
-// console.log(mensagem)
-// // a) Está pedindo para inserir um número
-// // b) Se fosse "10" a mensagem seria "Esse número passou no teste",
-// // se colocar "-10" não terá nenhuma mensagem
+console.log(mensagem)
+// a) Está pedindo para inserir um número
+// b) Se fosse "10" a mensagem seria "Esse número passou no teste",
+// se colocar "-10" não terá nenhuma mensagem
 
-// // Parte 2
-// // Exercício 1
-// // a)
-// const idade = Number (prompt("Insira sua idade"))
+// Parte 2
+// Exercício 1
+// a)
+const idade = Number (prompt("Insira sua idade"))
 
-// // b) c)
-// function verificarIdade(idade){
-//     if (idade >= 18 ){
-//     console.log('Você pode dirigir')
-//     } if (idade <= 17){
-//     console.log('Você não pode dirigir')
-//     }
-// }
-// verificarIdade(idade)
+// b) c)
+function verificarIdade(idade){
+    if (idade >= 18 ){
+    console.log('Você pode dirigir')
+    } if (idade <= 17){
+    console.log('Você não pode dirigir')
+    }
+}
+verificarIdade(idade)
 
 // Exerncício 2
 const turno = prompt('Qual seu turno? Responda com M para matutin, V para vespertino ou N para noturno!')
@@ -75,22 +75,32 @@ const turno = prompt('Qual seu turno? Responda com M para matutin, V para vesper
         console.log('Boa noite!');
     }
 
-// // Exercício 3
-// const turno = prompt('Qual seu turno?')
-// const verificaTurno = (turno)=> {
-//     switch(turno){
-//         case 'm':
-//             console.log('Bom dia');
-//             break;
-//         case 'v':
-//             console.log('Boa tarde')
-//             break;
-//         case 'n':
-//             console.log('Boa noite')
-//             break;
-//         default:
-//             console.log('Turno não encontrado!');
-//             break;
-//     }
-// }
-// verificaTurno(turno);
+// Exercício 3
+    switch(turno){
+        case 'm':
+            console.log('Bom dia');
+            break;
+        case 'v':
+            console.log('Boa tarde')
+            break;
+        case 'n':
+            console.log('Boa noite')
+            break;
+    }
+}
+verificaTurno(turno);
+
+// Exercício 4
+const genero = prompt('Qual gênero de filme assistiremos?').toUpperCase()
+const preco = prompt('Qual o preço do ingresso?').toUpperCase()
+if(genero === 'FANTASIA' && preco < 15){
+    console.log('Escolha outro filme :(')
+}
+
+// Desafio 1
+if (genero === 'FANTASIA' && preco <15 ){
+    lanche = prompt('Qual snack você vai comprar?')
+    console. log('Bom filme! \n Aproveite o seu filme', lanche)
+} else {
+    console.log('Escolha outro filme :(')
+}
