540eb23edc88ddf7dcbb57df74dbdbad5483771e
Henrique Dos Santos Rodriguez <henrique_rz@hotmail.com>
2022-07-05 17:32:26 -0300

    Agora está tudo certo

--- modulo3/use-effect/src/App.js
+++ modulo3/use-effect/src/App.js
@@ -1,15 +1,64 @@
-import Post from './components/Post';
-import React from 'react';
+import axios from "axios";
+import PokeCard from "./components/PokeCard";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // Passo 3b
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
+
+  // Passo 3c
+  const getPokeList = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        setPokeList(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getPokeList()
+  }, [pokeList]);
+
+  // Passo 3a
+  const changePokeName = (event) => {
+    // Passo 3d
+    setPokeName(event.target.value)
+  };
+
+  // Passo 3e
+  const pokeOptions = pokeList.map(pokemon => {
+    return (
+      <option key={pokemon.name} value={pokemon.name}>
+        {pokemon.name}
+      </option>
+    );
+  });
+
+  // Passo 4a
+  const pokemon = pokeName && <PokeCard pokeName={pokeName} />;
+
   return (
     <>
-      <h1>Post</h1>
-      <Post
-        nomeUsuario={'Usuário'}
-        fotoUsuario={'https://picsum.photos/50/55'}
-        fotoPost={'https://picsum.photos/200/156'}
-      />
+      <header>
+        <h1>Exibir Pokémon</h1>
+      </header>
+      <hr />
+      <nav>
+        <label htmlFor={"select-pokemon"}>Selecione um pokemon: </label>
+        {/* Passo 3a */}
+        <select onChange={changePokeName} id={"select-pokemon"} >
+          <option value={""}>Nenhum</option>
+          {/* Passo 3e */}
+          {pokeOptions}
+        </select>
+      </nav>
+      <main>
+        {pokemon}
+      </main>
     </>
   );
 };
--- modulo3/use-effect/src/index.js
+++ modulo3/use-effect/src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
